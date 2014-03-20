d0a3d6ac81ba67e0250fa684803dea35fd9f7d99
Caolan McMahon <caolan@caolanmcmahon.com>
2014-03-20 15:47:08 +0000

    add test for publishing as one user and reading as another

--- test/browser/test-example.js
+++ test/browser/test-example.js
@@ -13,4 +13,41 @@ suite('Browser API', function () {
     assert.ok(hoodie.global);
   });
 
+  test('publish as testuser1, read as testuser2', function (done) {
+    this.timeout(10000);
+    var docid;
+    var task = hoodie.account.signUp('testuser1', 'testing')
+      .then(function () {
+        return hoodie.store.add('test', {title: 'foo'}).publish()
+          .then(function (x) {
+            docid = x.id;
+            return x;
+          })
+      })
+      .then(function () {
+        return hoodie.account.signOut();
+      })
+      .then(function () {
+        return hoodie.account.signUp('testuser2', 'testing');
+      })
+      .then(function () {
+        // wait a bit to make sure the data has synced from the server
+        setTimeout(function () {
+          hoodie.store.find('test', docid)
+            .fail(function (err) {
+              assert.ok(false, err.message);
+              done();
+            })
+            .done(function (doc) {
+              assert.equal(doc.title, 'foo');
+              done();
+            })
+        }, 2000);
+      })
+      .fail(function (err) {
+        assert.ok(false, err.message);
+        done();
+      });
+  });
+
 });
